b8ffc80e5b50ae1b05581ca705882608467474df
Brendan Swiniarski <brendan.james.swiniarski@gmail.com>
2014-08-09 20:16:18 -0400

    Added support for the differences in linux and windows machines. When testing and coding in linux the graphical output was fine. However in windows the line thickness in chart 4, plot 4 (bottom right) was much greater, and made the output noticeably less clear. Also re-added code to remove box around the legend in chart 4 plot 3 (bottom left). Should have been committed with the last commit but somehow it went missing.

--- plot4.R
+++ plot4.R
@@ -7,8 +7,12 @@ run.plot4 <- function() {
     pc <- prepData()
     
     ##initialize a png graphic device, transparent background
-    ## default size is 480x480
-    png('plot4.png', bg="transparent")
+    ## default size is 480x480, type="cairo" let's line width be 0 < x < 1
+    ## this is not necessary in linux, as the default type works just fine
+    ## and produces a chart in the same quality, but when working on my 
+    ## windows device i was having trouble getting the line in the last plot
+    ## to be thin enough. this fixes that problem and works fine with linux as well.
+    png('plot4.png', bg="transparent", type="cairo")
     
     ##prepare a 2 x 2 grid of plots
     par(mfrow=c(2,2))
@@ -30,7 +34,7 @@ run.plot4 <- function() {
     #fourth plot, bottom right
     plot(pc$Global_reactive_power ~ pc$datetime, type="l",
          xlab="datetime", ylab="Global_reactive_power",
-         main="", lwd=1)
+         main="", lwd=.5)
     
     #close graphic device
     dev.off()
